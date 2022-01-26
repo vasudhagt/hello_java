6cc8549c3cab9c383c3316d56745c7e594e50c73
Vasudha <96834174+vasudhagt@users.noreply.github.com>
2022-01-26 17:06:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Happy republic day");
+    System.out.println("Happy republic day");
+    System.out.println("Happy republic day");
 
   }
 }
